009c1480c7a2e3164aa71d11f5cd8b3ced237594
MarcosFernandez24 <94877240+MarcosFernandez24@users.noreply.github.com>
2023-02-26 15:55:46 -0300

    tuve que cambiar algo

--- src/Components/Navbar Folder/CartWidget.jsx
+++ src/Components/Navbar Folder/CartWidget.jsx
@@ -3,7 +3,6 @@ import { BsFillCartFill } from "react-icons/bs";
 const CartWidget = () => {
   return (
     <div>
-      <span>Vacio</span>
       <BsFillCartFill color="purple" size={30} />
     </div>
   );
