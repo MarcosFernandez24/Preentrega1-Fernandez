e02e9626fbf99ecc82fde883dc66d9be66e9439f
MarcosFernandez24 <94877240+MarcosFernandez24@users.noreply.github.com>
2023-04-18 15:45:34 -0300

    Protegiendo el proyecto

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBzL6-K-_DM1Wx-XeMOPXY72l1PXt0-gP8",
-  authDomain: "ecomplay-react.firebaseapp.com",
-  projectId: "ecomplay-react",
-  storageBucket: "ecomplay-react.appspot.com",
-  messagingSenderId: "609474577850",
-  appId: "1:609474577850:web:012b4a580c159bf0a44983",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
